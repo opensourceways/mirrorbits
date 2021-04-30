20424d1c6f990550f0d221d8323bb0e63bc67e52
zhangjianjun_code <wusen@126.com>
2021-04-30 14:29:35 +0800

    Add mirror content through rpc and cancel the country field

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -276,7 +276,6 @@ func (c *CLI) AddMirror(ctx context.Context, in *Mirror) (*AddMirrorReply, error
 		reply.Latitude = geoRec.Latitude
 		reply.Longitude = geoRec.Longitude
 		reply.Continent = geoRec.ContinentCode
-		reply.Country = geoRec.Country
 		reply.ASN = fmt.Sprintf("%s (%d)", geoRec.ASName, geoRec.ASNum)
 	} else {
 		reply.Warnings = append(reply.Warnings,
