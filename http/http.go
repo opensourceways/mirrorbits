3fde557332d3562a2aa11ed86ee46321e3fce809
fengchao <feng0chao@gmail.com>
2024-12-26 21:05:13 +0800

    配置文件路径更新

--- http/http.go
+++ http/http.go
@@ -282,7 +282,7 @@ func (h *HTTP) mirrorHandler(w http.ResponseWriter, r *http.Request, ctx *Contex
 	}
 
 	// Sanitize path
-	urlPath, err := filesystem.EvaluateFilePath(cnf.Repository, editedUrlPath)
+	urlPath, err := filesystem.EvaluateFilePath(cnf.Repository+string(os.PathSeparator)+"sha", editedUrlPath)
 	if err != nil {
 		if err == filesystem.ErrOutsideRepo {
 			http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
